d0f25f25b4705d6f5970ff649a224030bf46a088
Jordan Valdez <150773615+Press1Start@users.noreply.github.com>
2024-01-29 18:57:20 -0800

    Initial commit

---
+++ src/weekOne.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class weekOne {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Math 140 week 1. What is your name?");
+        Scanner keyboard = new Scanner(System.in);
+        String name = keyboard.next();
+        System.out.println("Hello " + name + "!");
+
+    }
+}
